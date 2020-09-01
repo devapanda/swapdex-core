a03fe50382b578c841b22a1ab733269e140d9b00
deva panda <66827166+devapanda@users.noreply.github.com>
2020-09-01 16:08:38 +0530

    update

--- test/SwapdexPair.spec.ts
+++ test/SwapdexPair.spec.ts
@@ -15,7 +15,7 @@ const overrides = {
   gasLimit: 9999999
 }
 
-describe('SwapdexV2Pair', () => {
+describe('SwapdexPair', () => {
   const provider = new MockProvider({
     hardfork: 'istanbul',
     mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
